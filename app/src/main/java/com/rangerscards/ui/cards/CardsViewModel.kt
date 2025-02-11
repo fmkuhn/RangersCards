afc2fdec566cf7581df69fff254e8eab3e969789
Evgeny727 <genyakarelin23@gmail.com>
2025-02-11 15:41:18 +0300

    Remove unused import

--- app/src/main/java/com/rangerscards/ui/cards/CardsViewModel.kt
+++ app/src/main/java/com/rangerscards/ui/cards/CardsViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.rangerscards.data.Card
 import com.rangerscards.data.CardListItemProjection
 import com.rangerscards.data.CardsRepository
 import com.rangerscards.data.UserPreferencesRepository
